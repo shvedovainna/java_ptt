e14ff0b696d988f590542de1192e45a585246e06
shvedovainna <nega_san@mail.ru>
2018-03-14 23:47:43 +0300

    Лекция 2.8 Groups

--- addressbook-web-tests/src/test/java/ru/stqa/ptt/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptt/addressbook/appmanager/SessionHelper.java
@@ -3,20 +3,15 @@ package ru.stqa.ptt.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class SessionHelper {
-  private FirefoxDriver wd;
+public class SessionHelper extends HelperBase {
 
   public SessionHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void login(String user, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(user);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    type(By.name("user"), user);
+    type(By.name("pass"), password);
+    click(By.xpath("//form[@id='LoginForm']/input[3]"));
   }
 }
