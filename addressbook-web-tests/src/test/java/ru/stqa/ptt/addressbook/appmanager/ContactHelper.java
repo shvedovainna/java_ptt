62779094a622e435c7628c356db7955bf30d6da5
shvedovainna <nega_san@mail.ru>
2018-03-18 00:57:46 +0300

    Лекция 3.3

--- addressbook-web-tests/src/test/java/ru/stqa/ptt/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptt/addressbook/appmanager/ContactHelper.java
@@ -1,12 +1,13 @@
 package ru.stqa.ptt.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.ptt.addressbook.model.ContactData;
 
 public class ContactHelper extends HelperBase {
 
-  public ContactHelper(FirefoxDriver wd) {
+  public ContactHelper(WebDriver wd) {
     super(wd);
   }
 
--- addressbook-web-tests/src/test/java/ru/stqa/ptt/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptt/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.stqa.ptt.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.ptt.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
   @BeforeMethod
   public void setUp() throws Exception {
