644cfdea59a5316adde35621727073b15a0dcc99
shvedovainna <nega_san@mail.ru>
2018-03-22 15:00:41 +0300

    Лекция 3.10

--- addressbook-web-tests/src/test/java/ru/stqa/ptt/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptt/addressbook/tests/GroupCreationTests.java
@@ -9,6 +9,7 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().createGroup(new GroupData("Test1", null, null));
     app.getGroupHelper().initGroupCreation();
     app.getGroupHelper().fillGroupForm(new GroupData("Test1", null, null));
     app.getGroupHelper().submitGroupCreation();
